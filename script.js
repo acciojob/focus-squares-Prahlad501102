bd4a2bcb62fbad81166f83e0f7fe4f0a293381ba
PrahladKumar <prahladchoudhary7980@gmail.com>
2025-12-18 01:24:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(current => {
+  current.addEventListener("mouseenter", () => {
+    squares.forEach(square => {
+      if (square !== current) {
+        square.style.backgroundColor = "#6F4E37"; // coffee
+      }
+    });
+  });
+
+  current.addEventListener("mouseleave", () => {
+    squares.forEach(square => {
+      square.style.backgroundColor = "#E6E6FA"; // lavender
+    });
+  });
+});
